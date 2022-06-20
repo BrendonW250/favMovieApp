755ff10f758f7e2702974ced8fb687e058c750f1
BrendonW250 <brendonwright16@gmail.com>
2022-06-20 16:48:47 -0400

    try6

--- public/js/main.js
+++ public/js/main.js
@@ -19,8 +19,14 @@ function getFetch(){
             document.querySelector('.dateReleased').innerHTML = 'Release Date: ' + data.dateReleased
             // Celebs in movie
             document.querySelector('.celeb1').innerHTML = 'Name: ' + data.notableCelebs.name1
-            document.querySelector('.celeb2').innerHTML = 'Name: ' + data[notableCelebs].name2
-            document.querySelector('.celeb3').innerHTML = 'Name: ' + data[notableCelebs].name3
+            document.querySelector('.celeb2').innerHTML = 'Name: ' + data.notableCelebs.name2
+            document.querySelector('.celeb3').innerHTML = 'Name: ' + data.notableCelebs.name3
+
+            // for short summary of movie
+            document.querySelector('.movie-summary').innerHTML = 'Summary Of Movie: ' + data.shortBio
+
+            // for movie rating 
+            document.querySelector('.movie-rating').innerHTML = 'Rotten Tomatoes Rating: ' + data.rottenTomatoesRating
         })
 
         .catch(err => {
