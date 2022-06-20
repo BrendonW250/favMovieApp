4bff1257972db9b0c0f6824e79928f3b6916bf15
BrendonW250 <brendonwright16@gmail.com>
2022-06-20 15:55:12 -0400

    1stry

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,26 @@
 // client side js
-// document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('button').addEventListener('click', getFetch)
 
 
-// function getFetch(){
-//     const myFavs = document.querySelector('#enter-movie').value
+function getFetch(){
+    const myFavs = document.querySelector('#enter-movie').value
 
-//     const url = ''
-// }
+    const url = 'https://fav-movie-api.herokuapp.com/api'+myFavs
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+
+            // for I Am Legend
+            document.querySelector('.name-of-movie').innerText = 'Movie: ' + data.movieTitle
+            // Celebs in movie
+            document.querySelector('.name1').innerText = 'Name: ' + data.notableCelebs.name1
+            document.querySelector('.name2').innerText = 'Name: ' + data.notableCelebs.name2
+            document.querySelector('.name3').innerText = 'Name: ' + data.notableCelebs.name3
+        })
+
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+}
--- server.js
+++ server.js
@@ -6,8 +6,17 @@ const cors = require('cors')
 // so the app can use cors
 app.use(cors())
 
+// so the server can read the client side js
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+app.use(express.static('public'))
+
+
 const movies = {
     'I Am Legend': {
+        'movieTitle': 'I Am Legend',
         'dateReleased': 'December 14, 2007',
         'notableCelebs': {
             'name1': 'Will Smith',
@@ -17,10 +26,9 @@ const movies = {
         'shortBio': 'Will Smith plays as a scientist who survives a man-made plague that transforms humans into mutants. Now he wanders NY looking for survivors while on the hunt for a possible cure but the infected watch his every move hoping for his one mistake that will make Will become one of them',
         'rottenTomatoesRating': '68%'
         
-        
-        
     },
     'Avengers: Infinity War': {
+        'movieTitle': 'Avengers: Infinity War',
         'dateReleased': 'April 23, 2018',
         'notableCelebs': {
             'name1': 'Robert Downey Jr.',
@@ -32,6 +40,7 @@ const movies = {
         
     },
     'Fast & Furious: Tokyo Drift': {
+        'movieTitle': 'Fast & Furious: Tokyo Drift',
         'dateReleased': 'June 16, 2006',
         'notableCelebs': {
             'name1': 'Lucas Black',
@@ -43,6 +52,7 @@ const movies = {
 
     },
     'Really Love': {
+        'movieTitle': 'Really Love',
         'dateReleased': 'August 25, 2021',
         'notableCelebs': {
             'name1': 'Kofi Siriboe',
@@ -53,6 +63,7 @@ const movies = {
         'rottenTomatoesRating': '86%'
     },
     'Rush Hour 2': {
+        'movieTitle': 'Rush Hour 2',
         'dateReleased': 'August 3, 2001',
         'notableCelebs': {
             'name1': 'Jackie Chan',
@@ -63,6 +74,7 @@ const movies = {
         'rottenTomatoesRating': '52%'
     },
     'Other Movies': {
+        'movieTitle': 'Blahh The Movie',
         'dateReleased': 'May Blah Blah',
         'notableCelebs': {
             'name1': 'bre',
